ba3cf4dce41ae7bdeca3aaf75f0e898bcc0c51ab
ufolud71 <chudy.orfeusz@gmail.com>
2020-03-09 17:46:50 +0100

    Fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://https://mysterious-crag-07326.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://https://mysterious-crag-07326.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://mysterious-crag-07326.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://mysterious-crag-07326.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
