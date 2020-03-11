2f77b295cb618f56dbd93aad99ffb3d3feb06f27
ufolud71 <chudy.orfeusz@gmail.com>
2020-03-11 14:12:23 +0100

    Please...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://mysterious-crag-07326.herokuapp.com/v1/task/';
+  const apiRoot = 'https://mysterious-crag-07326.herokuapp.com/v1/';
   const trelloApiRoot = 'https://mysterious-crag-07326.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
