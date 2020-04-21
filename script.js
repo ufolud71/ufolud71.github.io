bcaa8da2b37fd35c2c36394bc7916adefb0c398f
ufolud71 <chudy.orfeusz@gmail.com>
2020-04-21 20:03:45 +0200

    Frontend

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://mysterious-crag-07326.herokuapp.com/v1/getTasks';
+  const apiRoot = 'https://mysterious-crag-07326.herokuapp.com/v1/task/getTasks';
   const trelloApiRoot = 'https://mysterious-crag-07326.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
